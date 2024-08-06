c22ccfbf30425941197334d74c6717d0c4747359
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-07 01:34:09 +0900

    chore: init project

---
+++ src/test/java/com/multirkh/chimhahaclone/ChimhahaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multirkh.chimhahaclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimhahaCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
