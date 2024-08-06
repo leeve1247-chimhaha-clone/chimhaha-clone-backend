8ec042b6fc936a9ecd11ff5bffde40c704b8aa53
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-07 01:34:09 +0900

    Init Project

---
+++ src/main/java/com/multirkh/chimhahaclone/ChimhahaCloneApplication.java
@@ -0,0 +1,13 @@
+package com.multirkh.chimhahaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimhahaCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChimhahaCloneApplication.class, args);
+    }
+
+}
