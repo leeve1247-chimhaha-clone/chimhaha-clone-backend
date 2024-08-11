3f58856e67a639a2aa17a4779db81b90f4a0f32e
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-11 10:00:03 +0900

    Update Post -> PostDto

--- src/main/java/com/multirkh/chimhahaclone/dto/PostDto.java
+++ src/main/java/com/multirkh/chimhahaclone/dto/PostDto.java
@@ -1,14 +1,24 @@
 // src/main/java/com/multirkh/chimhahaclone/dto/PostDto.java
 package com.multirkh.chimhahaclone.dto;
 
+import com.multirkh.chimhahaclone.entity.Post;
 import com.multirkh.chimhahaclone.entity.PostStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
-@AllArgsConstructor
 public class PostDto {
     private String title;
     private String username;
     private PostStatus status;
+    private Date createdDate;
+
+    public PostDto(Post post){
+        this.title = post.getTitle();
+        this.username = post.getUser().getUsername();
+        this.status = post.getStatus();
+        this.createdDate = post.getCreatedDate();
+    }
 }
