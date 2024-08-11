65b481f4968955c136fc7c27f2407aeba794630b
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-11 09:36:57 +0900

    Update Project Name

--- src/main/java/com/multirkh/chimhahaclone/config/CorsConfig.java
+++ src/main/java/com/multirkh/chimhahaclone/config/CorsConfig.java
@@ -3,15 +3,9 @@ package com.multirkh.chimhahaclone.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
