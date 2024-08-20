425329c07dd2b8cf4db19e240437df46bcfa0d46
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-20 21:11:13 +0900

    feat(category): add category

--- src/main/java/com/multirkh/chimhahaclone/dto/PostDto.java
+++ src/main/java/com/multirkh/chimhahaclone/dto/PostDto.java
@@ -2,8 +2,8 @@
 package com.multirkh.chimhahaclone.dto;
 
 import com.multirkh.chimhahaclone.entity.Post;
+import com.multirkh.chimhahaclone.category.PostCategory;
 import com.multirkh.chimhahaclone.entity.PostStatus;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Date;
@@ -14,11 +14,15 @@ public class PostDto {
     private String username;
     private PostStatus status;
     private Date createdDate;
+    private Integer views;
+    private PostCategory category;
 
     public PostDto(Post post){
         this.title = post.getTitle();
         this.username = post.getUser().getUsername();
         this.status = post.getStatus();
         this.createdDate = post.getCreatedDate();
+        this.views = post.getViews();
+        this.category = post.getCategory();
     }
 }
