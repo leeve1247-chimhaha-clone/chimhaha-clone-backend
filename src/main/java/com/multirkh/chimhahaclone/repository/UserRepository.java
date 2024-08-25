e56d8eda163a5ba2dfa623514941f7ecd6556c9d
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-26 07:03:08 +0900

    Update UserRepository.java

--- src/main/java/com/multirkh/chimhahaclone/repository/UserRepository.java
+++ src/main/java/com/multirkh/chimhahaclone/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.multirkh.chimhahaclone.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByUserName(String username);
+    List<User> findByEmail(String email);
 }
