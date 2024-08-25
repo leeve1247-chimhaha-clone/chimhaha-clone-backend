96ef59fa8b444091727b8a1110433d5498c04cd9
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-08-26 07:16:09 +0900

    feat(post): add query methods to post repository

--- src/main/java/com/multirkh/chimhahaclone/repository/PostRepository.java
+++ src/main/java/com/multirkh/chimhahaclone/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.multirkh.chimhahaclone.repository;
 
 import com.multirkh.chimhahaclone.entity.Post;
+import com.multirkh.chimhahaclone.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUser(User user);
+
 }
