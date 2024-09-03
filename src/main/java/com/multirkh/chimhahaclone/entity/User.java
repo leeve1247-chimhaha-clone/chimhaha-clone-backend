b2eacf660403c859781084bc041d7174b5ffb59c
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-04 06:41:36 +0900

    feat(user): add auth id field to user entity

--- src/main/java/com/multirkh/chimhahaclone/entity/User.java
+++ src/main/java/com/multirkh/chimhahaclone/entity/User.java
@@ -3,6 +3,7 @@ package com.multirkh.chimhahaclone.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -11,21 +12,22 @@ import java.util.List;
 @Getter
 @Setter
 @Table(name = "users")
+@NoArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String userName;
 
     @Column(nullable = false)
-    private String password;
-
-    @Column(nullable = false)
-    private String email;
+    private String userAuthId;
 
     @OneToMany(mappedBy = "user") // mappedBy: 연관관계의 주인이 아님을 나타냄 (읽기 전용) DB 에선 안보임
     private List<Post> posts;
+
+    public User(String userAuthId){
+        this.userAuthId = userAuthId;
+    }
 }
