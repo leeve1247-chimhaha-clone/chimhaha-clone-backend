8dcfd899c3c6ef88aef2821628902cfd9ef548fe
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-04 06:42:41 +0900

    refactor(post): update post controller

--- src/main/java/com/multirkh/chimhahaclone/controller/PostController.java
+++ src/main/java/com/multirkh/chimhahaclone/controller/PostController.java
@@ -1,8 +1,12 @@
 package com.multirkh.chimhahaclone.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.multirkh.chimhahaclone.category.PostCategory;
-import com.multirkh.chimhahaclone.category.repository.CategoryRepository;
 import com.multirkh.chimhahaclone.dto.PostDto;
+import com.multirkh.chimhahaclone.dto.PostReceived;
+import com.multirkh.chimhahaclone.entity.Post;
+import com.multirkh.chimhahaclone.entity.User;
+import com.multirkh.chimhahaclone.repository.PostCategoryRepository;
 import com.multirkh.chimhahaclone.repository.PostRepository;
 import com.multirkh.chimhahaclone.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +19,7 @@ import java.util.List;
 public class PostController {
 
     private final PostRepository postRepository;
-    private final CategoryRepository categoryRepository;
+    private final PostCategoryRepository postCategoryRepository;
     private final UserRepository userRepository;
 
     @GetMapping("/")
@@ -25,14 +29,27 @@ public class PostController {
 
     @GetMapping("/posts")
     public List<PostDto> getPosts(@RequestParam(name = "list_num", required = false, defaultValue = "0") int listNum) {
-        return postRepository.findAll().stream().map(PostDto::new).toList();
+        return postRepository.findAllByOrderByCreatedDateDesc().stream().map(PostDto::new).toList();
     }
 
     @PostMapping("/save")
     public String savePost(
             @RequestBody PostReceived request
     ) {
-        PostCategory postCategory = request.getPostCategory();
+        System.out.println("request = " + request);
+        System.out.println("request = " + request.getPostCategoryName());
+        PostCategory postCategory = postCategoryRepository.findByName(request.getPostCategoryName());
+        JsonNode jsonContent = request.getContent();
+        String userAuthId = request.getUser();
+        User user = userRepository.findByUserAuthId(userAuthId);
+        if(user == null){
+            user = new User(userAuthId);
+            userRepository.save(user);
+        }
+        System.out.println("jsonContent = " + jsonContent);
+        String title = request.getTitle();
+        Post post = new Post(title, jsonContent, user, postCategory);
+        Post save = postRepository.save(post);
         return "Post saved!";
     }
 }
