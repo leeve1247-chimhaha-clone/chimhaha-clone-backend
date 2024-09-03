a9eb7b5f1f5c61fe46240725606be1df50d18a90
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-04 06:41:13 +0900

    refactor(post): move post received class

---
+++ src/main/java/com/multirkh/chimhahaclone/dto/PostReceived.java
@@ -0,0 +1,16 @@
+package com.multirkh.chimhahaclone.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostReceived {
+    @JsonProperty("category")
+    private String postCategoryName;
+    private JsonNode content;
+    private String title;
+    private String user;
+}
