c8e8ed955455f84c50fa6648a931899bafd1abc3
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-06 01:19:54 +0900

    refactor(post): update post repository

--- src/main/java/com/multirkh/chimhahaclone/repository/PostRepository.java
+++ src/main/java/com/multirkh/chimhahaclone/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package com.multirkh.chimhahaclone.repository;
 
 import com.multirkh.chimhahaclone.entity.Post;
-import com.multirkh.chimhahaclone.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByUser(User user);
     List<Post> findAllByOrderByCreatedDateDesc();
 
 }
