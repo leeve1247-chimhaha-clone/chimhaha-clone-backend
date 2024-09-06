32a04e3ae321e0cfae5d058b18d9204e278bc041
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-06 14:44:20 +0900

    Add 답글 매핑

--- src/main/java/com/multirkh/chimhahaclone/dto/PostDetailDto.java
+++ src/main/java/com/multirkh/chimhahaclone/dto/PostDetailDto.java
@@ -32,6 +32,6 @@ public class PostDetailDto {
         this.likes = post.getLikes();
         this.postId = post.getId().intValue();
         this.content = post.getJsonContent();
-        this.comments = post.getComments().stream().map(CommentDto::new).toList();
+        this.comments = post.getComments().stream().filter(comment -> comment.getParent() == null).map(CommentDto::new).toList();
     }
 }
