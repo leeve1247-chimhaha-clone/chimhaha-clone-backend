ab40d1a4387ce1ec19d5e84846f9853bb9537fbd
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-06 14:44:20 +0900

    feat(comment): add reply mapping

--- src/main/java/com/multirkh/chimhahaclone/dto/CommentDto.java
+++ src/main/java/com/multirkh/chimhahaclone/dto/CommentDto.java
@@ -4,15 +4,22 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.multirkh.chimhahaclone.entity.Comment;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 public class CommentDto {
     private final String username;
     private final JsonNode content;
-    private Long id;
+    private final Long id;
+    private final List<CommentDto> children = new ArrayList<>();
 
     public CommentDto(Comment comment) {
         this.id = comment.getId();
         this.username = comment.getUser().getUserName();
         this.content = comment.getContent();
+        if (comment.getChildren() != null) {
+            comment.getChildren().forEach(child -> children.add(new CommentDto(child)));
+        }
     }
 }
