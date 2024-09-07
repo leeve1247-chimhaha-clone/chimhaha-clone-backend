afba074cdfa91976d7663196323faec8ec243453
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-08 03:58:27 +0900

    Minio 설정 추가

---
+++ src/main/java/com/multirkh/chimhahaclone/minio/MinioService.java
@@ -0,0 +1,42 @@
+package com.multirkh.chimhahaclone.minio;
+
+import com.multirkh.chimhahaclone.util.IdGenerator;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class MinioService {
+    @Value("${minio.export-url}")
+    private String minioExportUrl;
+    @Value("${minio.bucket-name}")
+    private String minioBucketName;
+
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    private final MinioClient minioClient;
+
+    public String postFileWithRandomFileName(MultipartFile file){
+        String randomImageName = IdGenerator.generateUniqueId();
+        try {
+            minioClient.putObject(
+                    PutObjectArgs.builder().bucket(minioBucketName).object(
+                                    randomImageName
+                            ).stream(file.getInputStream(), file.getSize(), -1)
+                            .contentType(file.getContentType())
+                            .build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return randomImageName;
+    }
+
+    public String getPreviewUrl(String randomImageName) {
+        return minioExportUrl + "/api/v1/buckets/" + minioBucketName + "/objects/download?preview=true&prefix=" + randomImageName;
+    }
+}
