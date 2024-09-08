4ccde40fadb71a02300d06d00871dad56807cc39
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-08 15:39:13 +0900

    feat(view-count): add view count feature using redis

---
+++ src/main/java/com/multirkh/chimhahaclone/redis/ViewCountService.java
@@ -0,0 +1,46 @@
+package com.multirkh.chimhahaclone.redis;
+
+import com.multirkh.chimhahaclone.entity.Post;
+import com.multirkh.chimhahaclone.repository.PostRepository;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class ViewCountService {
+
+    private final RedisTemplate<String, Integer> redisTemplate;
+    private final PostRepository postRepository;
+
+    public ViewCountService(RedisTemplate<String, Integer> redisTemplate, PostRepository postRepository) {
+        this.redisTemplate = redisTemplate;
+        this.postRepository = postRepository;
+    }
+
+    // 조회수 증가
+    public void incrementViewCount(Long postId) {
+        String key = "post:views:" + postId;
+        redisTemplate.opsForValue().increment(key, 1);
+    }
+
+    // 주기적으로 DB에 조회수를 반영하는 메소드
+    @Scheduled(fixedRate = 60000) // 1분마다 실행
+    public void syncViewCountsToDB() {
+        Set<String> keys = redisTemplate.keys("post:views:*");
+        if (keys != null) {
+            for (String key : keys) {
+                Long contentId = Long.valueOf(key.split(":")[2]);
+                Integer viewCount = redisTemplate.opsForValue().get(key);
+
+                if (viewCount != null) {
+                    Post post = postRepository.findById(contentId).orElseThrow(() -> new RuntimeException("Content not found"));
+                    post.setViews(post.getViews() + viewCount);
+                    postRepository.save(post);
+                    redisTemplate.delete(key); // DB에 반영한 후 Redis 에서 제거
+                }
+            }
+        }
+    }
+}
