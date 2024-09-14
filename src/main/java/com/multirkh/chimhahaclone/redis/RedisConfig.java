a2683906e00bfc56b8257653aa2ecba05c2f4477
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-14 11:40:57 +0900

    feat(view-count): enable scheduler and specify serialization logic

--- src/main/java/com/multirkh/chimhahaclone/redis/RedisConfig.java
+++ src/main/java/com/multirkh/chimhahaclone/redis/RedisConfig.java
@@ -1,23 +1,27 @@
 package com.multirkh.chimhahaclone.redis;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
 
-    @Bean
-    LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
+    private final RedisConnectionFactory redisConnectionFactory;
 
     @Bean
-    RedisTemplate<String, Integer> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<String, Integer> redisTemplate() {
         RedisTemplate<String, Integer> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 }
