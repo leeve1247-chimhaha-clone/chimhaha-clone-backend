e6e91bc1a226fee99f8155107b79b1105f1985c9
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-16 14:30:46 +0900

    test(image): add image value test case

--- src/test/java/com/multirkh/chimhahaclone/repository/PostRepositoryTest.java
+++ src/test/java/com/multirkh/chimhahaclone/repository/PostRepositoryTest.java
@@ -5,13 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.multirkh.chimhahaclone.bootup.DataInitializer;
 import com.multirkh.chimhahaclone.category.PostCategory;
 import com.multirkh.chimhahaclone.config.JwtDecoderTestConfig;
-import com.multirkh.chimhahaclone.config.MockImageController;
 import com.multirkh.chimhahaclone.config.TestPostRepository;
 import com.multirkh.chimhahaclone.dto.CommentDto;
 import com.multirkh.chimhahaclone.dto.PostDetailDto;
 import com.multirkh.chimhahaclone.entity.Comment;
 import com.multirkh.chimhahaclone.entity.Post;
 import com.multirkh.chimhahaclone.entity.User;
+import com.multirkh.chimhahaclone.minio.MinioConfig;
+import com.multirkh.chimhahaclone.service.ImageService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +22,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 import static com.multirkh.chimhahaclone.util.UtilStringJsonConverter.jsonNodeOf;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,9 +39,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Transactional
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PostRepositoryTest {
-    @MockBean
-    private MockImageController mockImageController;
-
+    @Autowired
+    private MinioConfig minioConfig;
     @Autowired
     private TestPostRepository postRepository;
     @Autowired
@@ -54,6 +54,8 @@ class PostRepositoryTest {
 
     @Autowired
     private ObjectMapper mapper = new ObjectMapper();
+    @Autowired
+    private ImageService imageService;
 
     @BeforeEach
     void insertData() {
@@ -61,10 +63,12 @@ class PostRepositoryTest {
         User user2 = userRepository.save(new User("bbron", "bbbb"));
         JsonNode jsonNode1 = jsonNodeOf("{\"ops\": [{\"insert\": \"post sample 1\\n\"}]}");
         JsonNode jsonNode2 = jsonNodeOf("{\"ops\": [{\"insert\": \"post sample 2\\n\"}]}");
+        JsonNode jsonNode3 = jsonNodeOf("{\"ops\": [{\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=NrND8bPKSOTPnvTlvpty.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": \"\\n\\n\", \"attributes\": {\"align\": \"center\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": \"\\n\\n\", \"attributes\": {\"align\": \"center\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": \"\\n\\n\", \"attributes\": {\"align\": \"right\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=3hv7ChhIgRMt1iPk0RKF.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=40rdJ4dWYhxHPsvfSfsm.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=2nMNo2892mYqfRTsE1cK.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=yw7S9KAS2E1R3CDwpaES.png\"}}, {\"insert\": \"\\n\"}]}");
         PostCategory categoryHobby = postCategoryRepository.findByName("HOBBY");
 
         Post post1 = postRepository.save(new Post("Test Post 3", jsonNode1, user1, categoryHobby, ""));
         Post post2 = postRepository.save(new Post("Test Post 4", jsonNode1, user1, categoryHobby, ""));
+        Post post3 = postRepository.save(new Post("Test Post 5", jsonNode3, user2, categoryHobby, ""));
 
         commentRepository.save(new Comment(jsonNodeOf("{\"ops\": [{\"insert\": \"comment sample 1\\n\"}]}"), post1, user1, 0));
         commentRepository.save(new Comment(jsonNodeOf("{\"ops\": [{\"insert\": \"comment sample 2\\n\"}]}"), post1, user1, 0));
@@ -133,6 +137,15 @@ class PostRepositoryTest {
         JsonNode ops = content.get("ops");
         log.info(String.valueOf(content));
         log.info(String.valueOf(ops));
+    }
+
+    @Test
+    public void getImages(){
+        Post post = postRepository.findByTitleEquals("Test Post 5").orElseThrow(() -> new EntityNotFoundException("Post not found"));
+        Set<String> images = imageService.getImageUrls(post.getJsonContent());
+        for(String image : images){
+            log.info(image);
+        }
     }
 
 
