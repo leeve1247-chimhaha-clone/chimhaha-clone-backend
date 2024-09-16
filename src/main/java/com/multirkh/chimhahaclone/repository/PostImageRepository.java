3296613a4dff0cdc41fe8cabe9ad6a413a86f8c8
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-16 16:46:29 +0900

    feat(image): add PostImage entity

---
+++ src/main/java/com/multirkh/chimhahaclone/repository/PostImageRepository.java
@@ -0,0 +1,7 @@
+package com.multirkh.chimhahaclone.repository;
+
+import com.multirkh.chimhahaclone.entity.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+}
