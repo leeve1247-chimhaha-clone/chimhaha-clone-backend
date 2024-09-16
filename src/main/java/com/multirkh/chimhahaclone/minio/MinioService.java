fef2d8e8d90ce4888c1f7f7734079e4641fd27a0
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-16 22:24:24 +0900

    [덤프 이미지 삭제]: MinioService 이미지 삭제 기능 추가

--- src/main/java/com/multirkh/chimhahaclone/minio/MinioService.java
+++ src/main/java/com/multirkh/chimhahaclone/minio/MinioService.java
@@ -1,15 +1,19 @@
 package com.multirkh.chimhahaclone.minio;
 
 import com.multirkh.chimhahaclone.util.IdGenerator;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
+import io.minio.messages.DeleteError;
+import io.minio.messages.DeleteObject;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -40,5 +44,36 @@ public class MinioService {
     // randomImageFileName : "파일이름 명.확장자" ex : "this_is_random_string_20.png"
     public String getPreviewUrl(String randomImageFileName) {
         return minioExportUrl + "/api/v1/buckets/" + minioBucketName + "/objects/download?preview=true&prefix=" + randomImageFileName;
+    }
+
+    public void deleteImages(Set<String> fileNames) {
+        List<DeleteObject> objects = new LinkedList<>();
+        for (String fileName : fileNames) {
+            objects.add(new DeleteObject(fileName));
+        }
+        try{
+            Iterable<Result<DeleteError>> results =
+                    minioClient.removeObjects(
+                            RemoveObjectsArgs.builder().bucket("my-bucketname").objects(objects).build());
+            for (Result<DeleteError> result : results) {
+                DeleteError error = result.get();
+                System.out.println(
+                        "Error in deleting object " + error.objectName() + "; " + error.message());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteImage(String fileName) {
+        try {
+            minioClient.removeObject(
+                    RemoveObjectArgs.builder()
+                            .bucket(minioBucketName)
+                            .object(fileName)
+                            .build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
