d3e29e7a086bb7e86a8a2a53549a1d839baa5dde
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-16 21:55:00 +0900

    feat(image): add post image update to image service

--- src/main/java/com/multirkh/chimhahaclone/service/ImageService.java
+++ src/main/java/com/multirkh/chimhahaclone/service/ImageService.java
@@ -5,7 +5,6 @@ import com.multirkh.chimhahaclone.entity.Image;
 import com.multirkh.chimhahaclone.entity.ImageStatus;
 import com.multirkh.chimhahaclone.entity.Post;
 import com.multirkh.chimhahaclone.entity.PostImage;
-import com.multirkh.chimhahaclone.minio.MinioService;
 import com.multirkh.chimhahaclone.repository.ImageRepository;
 import com.multirkh.chimhahaclone.repository.PostImageRepository;
 import org.springframework.stereotype.Service;
@@ -19,9 +18,11 @@ import java.util.Set;
 public class ImageService {
 
     private final ImageRepository imageRepository;
+    private final PostImageRepository postImageRepository;
 
-    public ImageService(ImageRepository imageRepository, PostImageRepository postImageRepository, MinioService minioService) {
+    public ImageService(ImageRepository imageRepository, PostImageRepository postImageRepository) {
         this.imageRepository = imageRepository;
+        this.postImageRepository = postImageRepository;
     }
 
     public Set<String> getImageUrls(JsonNode jsonContent) {
@@ -35,14 +36,45 @@ public class ImageService {
     public void createPostImages(Post post, JsonNode jsonContent) {
         Set<String> imageUrls = getImageUrls(jsonContent);
         Set<PostImage> postImages = new HashSet<>();
-
         if (imageUrls.isEmpty()) return;
-
         Set<Image> images = imageRepository.findByFileNames(imageUrls);
         for (Image image : images) {
             PostImage postImage = new PostImage(post, image, ImageStatus.POSTED);
             postImages.add(postImage);
         }
         post.getPostImages().addAll(postImages);
+    }
+
+    public void updatePostImage(Post post, JsonNode jsonContent) {
+        Set<PostImage> postImages = postImageRepository.findByPost(post);
+        Set<String> newImageUrls = getImageUrls(jsonContent);
+
+        Set<PostImage> deletedPostImages = new HashSet<>();
+        Set<Image> deletedImages = new HashSet<>();
+        postImages.forEach(postImage -> {
+            Image image = postImage.getImage();
+            if (!newImageUrls.contains(image.getFileName())) {
+                Set<PostImage> postImagesOfImage = image.getPostImages();
+                postImagesOfImage.remove(postImage);
+                if (postImagesOfImage.isEmpty()) {
+                    //minio 이미지 삭제
+                    deletedImages.add(image);
+                }
+                deletedPostImages.add(postImage);
+            } else {
+                postImage.setStatus(ImageStatus.POSTED);
+                newImageUrls.remove(image.getFileName());
+            }
+        });
+
+        postImageRepository.deleteAllByPostImages(deletedPostImages);
+        imageRepository.deleteAllByImages(deletedImages);
+
+        postImages.removeAll(deletedPostImages);
+        Set<Image> images = imageRepository.findByFileNames(newImageUrls);
+        for (Image image : images) {
+            PostImage postImage = new PostImage(post, image, ImageStatus.POSTED);
+            postImages.add(postImage);
+        }
     }
 }
