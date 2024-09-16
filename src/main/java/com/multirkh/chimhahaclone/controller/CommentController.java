92cd46568a4861b9cb973195a0b2fbeb25ca9a9c
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-16 11:21:56 +0900

    [좋아요 기능] 컨트롤러 추가

--- src/main/java/com/multirkh/chimhahaclone/controller/CommentController.java
+++ src/main/java/com/multirkh/chimhahaclone/controller/CommentController.java
@@ -1,20 +1,13 @@
 package com.multirkh.chimhahaclone.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.multirkh.chimhahaclone.entity.Comment;
-import com.multirkh.chimhahaclone.entity.Post;
-import com.multirkh.chimhahaclone.entity.User;
-import com.multirkh.chimhahaclone.repository.CommentRepository;
-import com.multirkh.chimhahaclone.repository.PostCategoryRepository;
-import com.multirkh.chimhahaclone.repository.PostRepository;
-import com.multirkh.chimhahaclone.repository.UserRepository;
+import com.multirkh.chimhahaclone.entity.*;
+import com.multirkh.chimhahaclone.repository.*;
 import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +18,7 @@ public class CommentController {
     private final PostCategoryRepository postCategoryRepository;
     private final UserRepository userRepository;
     private final CommentRepository commentRepository;
+    private final CommentLikesUserRepository commentLikesUserRepository;
 
     @PostMapping("/save/comment")
     @RolesAllowed("USER")
@@ -39,12 +33,34 @@ public class CommentController {
         if (request.getCommentId() == null) {
             Comment comment = new Comment(jsonContent, post, user, 0);
             commentRepository.save(comment);
-        }
-        else {
+        } else {
             Comment parent = commentRepository.findById(request.getCommentId()).orElse(null);
             Comment comment = new Comment(jsonContent, post, user, 0, parent);
             commentRepository.save(comment);
         }
         return "Your comment has saved very well";
+    }
+
+    @GetMapping("/comments/like")
+    @RolesAllowed("USER")
+    public String likeComment(
+            @RequestParam(name = "num", defaultValue = "0") Long listNum
+    ) {
+        String user_auth_id = SecurityContextHolder.getContext().getAuthentication().getName();
+        Comment comment = commentRepository.findById(listNum).orElseThrow(() -> new IllegalArgumentException("post not found"));
+        User user = userRepository.findByUserAuthId(user_auth_id);
+        CommentLikesUser commentLikesUser = commentLikesUserRepository.findByCommentAndUser(comment, user);
+        if (commentLikesUser == null) {
+            comment.setLikes(comment.getLikes() + 1);
+            commentLikesUser = new CommentLikesUser(comment, user);
+            commentLikesUserRepository.save(commentLikesUser);
+            commentRepository.save(comment);
+            return "liked";
+        } else {
+            comment.setLikes(comment.getLikes() - 1);
+            commentLikesUserRepository.delete(commentLikesUser);
+            commentRepository.save(comment);
+            return "like canceled";
+        }
     }
 }
