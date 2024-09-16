f955b8d8c106d000471571ae5dfdf00ae0c15a55
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-17 07:45:52 +0900

    feat(like): update entity relationships for like feature

--- src/main/java/com/multirkh/chimhahaclone/entity/CommentLikesUser.java
+++ src/main/java/com/multirkh/chimhahaclone/entity/CommentLikesUser.java
@@ -4,10 +4,12 @@ package com.multirkh.chimhahaclone.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "comment_likes_user")
 public class CommentLikesUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,8 +21,13 @@ public class CommentLikesUser {
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
+    @Setter
+    @Column(name = "comment_like") // like 는 예약어이므로 컬럼명을 comment_like 로 지정
+    private Boolean like;
+
     public CommentLikesUser(Comment comment, User user) {
         this.comment = comment;
         this.user = user;
+        this.like = true;
     }
 }
--- src/main/java/com/multirkh/chimhahaclone/entity/PostLikesUser.java
+++ src/main/java/com/multirkh/chimhahaclone/entity/PostLikesUser.java
@@ -3,10 +3,12 @@ package com.multirkh.chimhahaclone.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "post_likes_user")
 public class PostLikesUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,8 +20,13 @@ public class PostLikesUser {
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
+    @Setter
+    @Column(name = "post_like") // like 는 예약어이므로 컬럼명을 post_like 로 지정
+    private Boolean like;
+
     public PostLikesUser(Post post, User user) {
         this.post = post;
         this.user = user;
+        this.like = true;
     }
 }
