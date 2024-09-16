8b677c179f5441c0d712abf8bbc7e924e8af146f
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-16 16:47:50 +0900

    feat(image): add status tag on temp image creation

--- src/main/java/com/multirkh/chimhahaclone/controller/ImageController.java
+++ src/main/java/com/multirkh/chimhahaclone/controller/ImageController.java
@@ -1,6 +1,9 @@
 package com.multirkh.chimhahaclone.controller;
 
+import com.multirkh.chimhahaclone.entity.Image;
+import com.multirkh.chimhahaclone.entity.ImageStatus;
 import com.multirkh.chimhahaclone.minio.MinioService;
+import com.multirkh.chimhahaclone.repository.ImageRepository;
 import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +15,15 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class ImageController {
     private final MinioService minioService;
+    private final ImageRepository imageRepository;
 
     @RolesAllowed("USER")
     @PostMapping("/upload/image")
     public String uploadFile(@RequestParam("file") MultipartFile file) {
+        // save into minio
         String randomImageName = minioService.postFileWithRandomFileName(file);
+        // save into db
+        imageRepository.save(new Image(randomImageName, ImageStatus.TEMPORARY));
         return minioService.getPreviewUrl(randomImageName);
     }
 }
