86d2e39d1f87adbb4b4e6b75af0a755de850d46f
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-16 21:55:49 +0900

    fix(image): fix repository query for unused image deletion

--- src/main/java/com/multirkh/chimhahaclone/repository/ImageRepository.java
+++ src/main/java/com/multirkh/chimhahaclone/repository/ImageRepository.java
@@ -2,6 +2,7 @@ package com.multirkh.chimhahaclone.repository;
 
 import com.multirkh.chimhahaclone.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Set;
@@ -11,4 +12,8 @@ public interface ImageRepository extends JpaRepository<Image, Long> {
 
     @Query("SELECT i FROM Image i WHERE i.fileName IN :fileNames")
     Set<Image> findByFileNames(Set<String> fileNames);
+
+    @Modifying
+    @Query("DELETE FROM Image i WHERE i in :images")
+    void deleteAllByImages(Set<Image> images);
 }
--- src/main/java/com/multirkh/chimhahaclone/repository/PostImageRepository.java
+++ src/main/java/com/multirkh/chimhahaclone/repository/PostImageRepository.java
@@ -1,7 +1,19 @@
 package com.multirkh.chimhahaclone.repository;
 
+import com.multirkh.chimhahaclone.entity.Post;
 import com.multirkh.chimhahaclone.entity.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
 
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+
+    @Query("SELECT pi FROM PostImage pi left join fetch pi.post p left join fetch pi.image i left join fetch i.postImages ipi WHERE pi.post = :post")
+    Set<PostImage> findByPost(Post post);
+
+    @Modifying
+    @Query("DELETE FROM PostImage pi WHERE pi in :postImages")
+    void deleteAllByPostImages(Set<PostImage> postImages);
 }
