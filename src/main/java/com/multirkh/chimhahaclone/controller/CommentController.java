550aee32cc74ee1c65bf019fdfdd51f5ba3df821
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-17 07:47:04 +0900

    [좋아요 기능] 댓글 좋아요 기능 추가

--- src/main/java/com/multirkh/chimhahaclone/controller/CommentController.java
+++ src/main/java/com/multirkh/chimhahaclone/controller/CommentController.java
@@ -1,13 +1,23 @@
 package com.multirkh.chimhahaclone.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.multirkh.chimhahaclone.entity.*;
+import com.multirkh.chimhahaclone.dto.CommentDto;
+import com.multirkh.chimhahaclone.entity.Comment;
+import com.multirkh.chimhahaclone.entity.CommentLikesUser;
+import com.multirkh.chimhahaclone.entity.Post;
+import com.multirkh.chimhahaclone.entity.User;
 import com.multirkh.chimhahaclone.repository.*;
 import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import static com.multirkh.chimhahaclone.util.UtilStringJsonConverter.jsonNodeOf;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,34 +30,69 @@ public class CommentController {
     private final CommentRepository commentRepository;
     private final CommentLikesUserRepository commentLikesUserRepository;
 
+    @PostMapping("/update/comment")
+    @RolesAllowed("USER")
+    public CommentDto updateComment(
+            @RequestBody CommentReceived request
+    ) {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUserAuthId(name);
+        Comment comment = commentRepository.findById(request.getCommentId()).orElseThrow();
+
+        if (comment.getUser().getId().equals(user.getId())) {
+            comment.setContent(request.getContent());
+            commentRepository.save(comment);
+            return new CommentDto(comment);
+        } else {
+            throw new IllegalArgumentException("You are not the owner of this comment");
+        }
+    }
+
+    @PostMapping("/delete/comment")
+    @RolesAllowed("USER")
+    public CommentDto deleteComment(
+            @RequestBody CommentReceived request
+    ) {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByUserAuthId(name);
+        Comment comment = commentRepository.findById(request.getCommentId()).orElseThrow();
+        if (comment.getUser().getId().equals(user.getId())) {
+            comment.setContent(jsonNodeOf("{\"ops\": [{\"insert\": \"삭제된 댓글입니다\\n\"}]}"));
+            commentRepository.save(comment);
+            return new CommentDto(comment);
+        } else {
+            throw new IllegalArgumentException("You are not the owner of this comment");
+        }
+    }
+
     @PostMapping("/save/comment")
     @RolesAllowed("USER")
-    public String saveComment(
+    public CommentDto saveComment(
             @RequestBody CommentReceived request
     ) {
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
         User user = userRepository.findByUserAuthId(name);
         JsonNode jsonContent = request.getContent();
-        if (postRepository.findById(request.getPostId()).isEmpty()) return "There is no post";
-        Post post = postRepository.findById(request.getPostId()).get();
+        Post post = postRepository.findById(request.getPostId()).orElseThrow();
         if (request.getCommentId() == null) {
             Comment comment = new Comment(jsonContent, post, user, 0);
             commentRepository.save(comment);
+            return new CommentDto(comment);
         } else {
             Comment parent = commentRepository.findById(request.getCommentId()).orElse(null);
             Comment comment = new Comment(jsonContent, post, user, 0, parent);
             commentRepository.save(comment);
+            return new CommentDto(comment);
         }
-        return "Your comment has saved very well";
     }
 
-    @GetMapping("/comments/like")
+    @PostMapping("/comments/like")
     @RolesAllowed("USER")
-    public String likeComment(
-            @RequestParam(name = "num", defaultValue = "0") Long listNum
+    public Integer likeComment(
+            @RequestBody Map<String, Long> body
     ) {
         String user_auth_id = SecurityContextHolder.getContext().getAuthentication().getName();
-        Comment comment = commentRepository.findById(listNum).orElseThrow(() -> new IllegalArgumentException("post not found"));
+        Comment comment = commentRepository.findById(body.get("num")).orElseThrow(() -> new IllegalArgumentException("post not found"));
         User user = userRepository.findByUserAuthId(user_auth_id);
         CommentLikesUser commentLikesUser = commentLikesUserRepository.findByCommentAndUser(comment, user);
         if (commentLikesUser == null) {
@@ -55,12 +100,19 @@ public class CommentController {
             commentLikesUser = new CommentLikesUser(comment, user);
             commentLikesUserRepository.save(commentLikesUser);
             commentRepository.save(comment);
-            return "liked";
+            return comment.getLikes();
         } else {
-            comment.setLikes(comment.getLikes() - 1);
-            commentLikesUserRepository.delete(commentLikesUser);
-            commentRepository.save(comment);
-            return "like canceled";
+            if (commentLikesUser.getLike()) {
+                commentLikesUser.setLike(false);
+                comment.setLikes(comment.getLikes() - 1);
+                commentRepository.save(comment);
+                return comment.getLikes();
+            } else {
+                commentLikesUser.setLike(true);
+                comment.setLikes(comment.getLikes() + 1);
+                commentRepository.save(comment);
+                return comment.getLikes();
+            }
         }
     }
 }
