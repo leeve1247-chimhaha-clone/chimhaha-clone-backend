3825a310d1a33e6a5128b51dbb3a84d93e06afa3
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-17 07:46:38 +0900

    feat(like): add comment like entity

--- src/main/java/com/multirkh/chimhahaclone/dto/CommentDto.java
+++ src/main/java/com/multirkh/chimhahaclone/dto/CommentDto.java
@@ -12,12 +12,14 @@ public class CommentDto {
     private final String username;
     private final JsonNode content;
     private final Long id;
+    private final Integer likes;
     private final List<CommentDto> children = new ArrayList<>();
 
     public CommentDto(Comment comment) {
         this.id = comment.getId();
         this.username = comment.getUser().getUserName();
         this.content = comment.getContent();
+        this.likes = comment.getLikes();
         if (comment.getChildren() != null) {
             comment.getChildren().forEach(child -> children.add(new CommentDto(child)));
         }
