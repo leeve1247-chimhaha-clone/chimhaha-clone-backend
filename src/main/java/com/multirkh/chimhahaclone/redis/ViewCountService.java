5f9133afa342f32ecdbfd8d76edf29611a2e24ea
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-16 09:41:38 +0900

    fix(view-count): handle orphan key exception and disable log

--- src/main/java/com/multirkh/chimhahaclone/redis/ViewCountService.java
+++ src/main/java/com/multirkh/chimhahaclone/redis/ViewCountService.java
@@ -2,12 +2,14 @@ package com.multirkh.chimhahaclone.redis;
 
 import com.multirkh.chimhahaclone.entity.Post;
 import com.multirkh.chimhahaclone.repository.PostRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Slf4j
 @Service
 public class ViewCountService {
 
@@ -22,20 +24,28 @@ public class ViewCountService {
     // 조회수 증가
     public void incrementViewCount(Long postId) {
         String key = "post:views:" + postId;
+        // log.info("key = {}", key);
         redisTemplate.opsForValue().increment(key);
     }
 
     // 주기적으로 DB에 조회수를 반영하는 메소드
+
     @Scheduled(fixedRate = 3000) // 1초마다 실행
     public void syncViewCountsToDB() {
+        // log.info("syncViewCountsToDB() 실행");
         Set<String> keys = redisTemplate.keys("post:views:*");
         if (keys != null) {
             for (String key : keys) {
                 Long contentId = Long.valueOf(key.split(":")[2]);
                 Integer viewCount = redisTemplate.opsForValue().get(key);
+                // log.info("key = {}, viewCount = {}, contendId = {}", key, viewCount, contentId);
 
                 if (viewCount != null) {
-                    Post post = postRepository.findById(contentId).orElseThrow(() -> new RuntimeException("Content not found"));
+                    Post post = postRepository.findById(contentId).orElse(null);
+                    if (post == null) {
+                        redisTemplate.delete(key);
+                        continue;
+                    }
                     post.setViews(post.getViews() + viewCount);
                     postRepository.save(post);
                     redisTemplate.delete(key); // DB에 반영한 후 Redis 에서 제거
