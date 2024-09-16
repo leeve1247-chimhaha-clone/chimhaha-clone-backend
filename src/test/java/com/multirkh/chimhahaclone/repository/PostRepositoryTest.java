d5b477e7dba2dea51ac21376cfa7d922e331575b
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-16 21:54:06 +0900

    [테스트 케이스]  updateImages 테스트 케이스 추가 및 버그 수정

--- src/test/java/com/multirkh/chimhahaclone/repository/PostRepositoryTest.java
+++ src/test/java/com/multirkh/chimhahaclone/repository/PostRepositoryTest.java
@@ -5,21 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.multirkh.chimhahaclone.bootup.DataInitializer;
 import com.multirkh.chimhahaclone.category.PostCategory;
 import com.multirkh.chimhahaclone.config.JwtDecoderTestConfig;
-import com.multirkh.chimhahaclone.config.TestPostRepository;
 import com.multirkh.chimhahaclone.dto.CommentDto;
 import com.multirkh.chimhahaclone.dto.PostDetailDto;
-import com.multirkh.chimhahaclone.entity.Comment;
-import com.multirkh.chimhahaclone.entity.Post;
-import com.multirkh.chimhahaclone.entity.User;
+import com.multirkh.chimhahaclone.entity.*;
 import com.multirkh.chimhahaclone.minio.MinioConfig;
 import com.multirkh.chimhahaclone.service.ImageService;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -27,6 +21,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import static com.multirkh.chimhahaclone.util.UtilStringJsonConverter.jsonNodeOf;
@@ -42,7 +37,7 @@ class PostRepositoryTest {
     @Autowired
     private MinioConfig minioConfig;
     @Autowired
-    private TestPostRepository postRepository;
+    private PostRepository postRepository;
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -56,32 +51,52 @@ class PostRepositoryTest {
     private ObjectMapper mapper = new ObjectMapper();
     @Autowired
     private ImageService imageService;
+    @Autowired
+    private ImageRepository imageRepository;
+    @Autowired
+    private PostImageRepository postImageRepository;
 
     @BeforeEach
     void insertData() {
+        imageRepository.save(new Image("1.png", ImageStatus.POSTED));
+        imageRepository.save(new Image("2.png", ImageStatus.POSTED));
+        imageRepository.save(new Image("3.png", ImageStatus.POSTED));
+        imageRepository.save(new Image("4.png", ImageStatus.POSTED));
+        imageRepository.save(new Image("5.png", ImageStatus.POSTED));
+        imageRepository.save(new Image("6.png", ImageStatus.POSTED));
+
+        imageRepository.save(new Image("7.png", ImageStatus.POSTED));
+        imageRepository.save(new Image("8.png", ImageStatus.POSTED));
+
+
         User user1 = userRepository.save(new User("aaron", "aaaa"));
         User user2 = userRepository.save(new User("bbron", "bbbb"));
         JsonNode jsonNode1 = jsonNodeOf("{\"ops\": [{\"insert\": \"post sample 1\\n\"}]}");
-        JsonNode jsonNode2 = jsonNodeOf("{\"ops\": [{\"insert\": \"post sample 2\\n\"}]}");
-        JsonNode jsonNode3 = jsonNodeOf("{\"ops\": [{\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=NrND8bPKSOTPnvTlvpty.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": \"\\n\\n\", \"attributes\": {\"align\": \"center\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": \"\\n\\n\", \"attributes\": {\"align\": \"center\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=RBbVllH7HRQ3RnF4udR4.png\"}}, {\"insert\": \"\\n\\n\", \"attributes\": {\"align\": \"right\"}}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=3hv7ChhIgRMt1iPk0RKF.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=40rdJ4dWYhxHPsvfSfsm.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=2nMNo2892mYqfRTsE1cK.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://192.168.1.14:1191/api/v1/buckets/my-bucket/objects/download?preview=true&prefix=yw7S9KAS2E1R3CDwpaES.png\"}}, {\"insert\": \"\\n\"}]}");
+        JsonNode jsonNode2 = jsonNodeOf("{\"ops\": [{\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=6.png\"}}, {\"insert\": \"\\n\"}]}");
+        JsonNode jsonNode3 = jsonNodeOf("{\"ops\": [{\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=1.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=2.png\"}}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=2.png\"}}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=2.png\"}}, {\"insert\": \"\\n\\n\", \"attributes\": {\"align\": \"center\"}}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=2.png\"}}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=2.png\"}}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=2.png\"}}, {\"insert\": \"\\n\\n\", \"attributes\": {\"align\": \"center\"}}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=2.png\"}}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=2.png\"}}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=2.png\"}}, {\"insert\": \"\\n\\n\", \"attributes\": {\"align\": \"right\"}}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=3.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=4.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=5.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=6.png\"}}, {\"insert\": \"\\n\"}]}");
         PostCategory categoryHobby = postCategoryRepository.findByName("HOBBY");
 
-        Post post1 = postRepository.save(new Post("Test Post 3", jsonNode1, user1, categoryHobby, ""));
-        Post post2 = postRepository.save(new Post("Test Post 4", jsonNode1, user1, categoryHobby, ""));
-        Post post3 = postRepository.save(new Post("Test Post 5", jsonNode3, user2, categoryHobby, ""));
+        Post post1 = postRepository.save(new Post("Test Post 1", jsonNode1, user1, categoryHobby, ""));
+        Post post2 = postRepository.save(new Post("Test Post 2", jsonNode1, user1, categoryHobby, ""));
+        Post post3 = postRepository.save(new Post("Test Post 3", jsonNode3, user2, categoryHobby, ""));
 
         commentRepository.save(new Comment(jsonNodeOf("{\"ops\": [{\"insert\": \"comment sample 1\\n\"}]}"), post1, user1, 0));
         commentRepository.save(new Comment(jsonNodeOf("{\"ops\": [{\"insert\": \"comment sample 2\\n\"}]}"), post1, user1, 0));
         commentRepository.save(new Comment(jsonNodeOf("{\"ops\": [{\"insert\": \"comment sample 3\\n\"}]}"), post1, user2, 0));
         commentRepository.save(new Comment(jsonNodeOf("{\"ops\": [{\"insert\": \"comment sample 4\\n\"}]}"), post2, user1, 0));
         commentRepository.save(new Comment(jsonNodeOf("{\"ops\": [{\"insert\": \"comment sample 5\\n\"}]}"), post2, user2, 0));
+        imageService.createPostImages(post2, jsonNode2);
+        imageService.createPostImages(post3, jsonNode3);
+        
         log.info(String.valueOf(System.identityHashCode(post1)));
         em.flush(); // db commit
         em.clear(); // clear entities
+        log.info("post3 size is {}",post3.getPostImages().size());
         log.info("===============End Before Init=================");
     }
 
     @Test
+    @Order(1)
     void testCreatePost() {
         User user = userRepository.save(new User("ccron", "cccc"));
         String title = "Test Post3";
@@ -93,27 +108,30 @@ class PostRepositoryTest {
     }
 
     @Test
+    @Order(1)
     void loadPostDto() {
-        Post post = postRepository.findByTitleEquals("Test Post 3").orElseThrow();
-        log.info(String.valueOf(post.getJsonContent()));
+        Assertions.assertThrows(NoSuchElementException.class, () -> {postRepository.findByTitleEquals("Test Post 1").orElseThrow();});
+
     }
 
     @Test
+    @Order(1)
     void loadPostDetailDto() {
         List<Comment> commentList = commentRepository.findAll();
-        Post post = postRepository.findByTitleEquals("Test Post 3").orElseThrow(() -> new EntityNotFoundException("Post not found"));
+        Post post = postRepository.findByTitleEquals("Test Post 1").orElseThrow(() -> new EntityNotFoundException("Post not found"));
         for (Comment comment : commentList) {
             log.info(String.valueOf(comment.getContent()));
         }
     }
 
     @Test
+    @Order(1)
     public void testGetPostWithComments() {
         // BeforeEach 의 post 와 동일한 post 를 참조함
-        Post post = postRepository.findByTitleEquals("Test Post 3").orElseThrow(() -> new EntityNotFoundException("Post not found"));
+        Post post = postRepository.findByTitleEquals("Test Post 1").orElseThrow(() -> new EntityNotFoundException("Post not found"));
         log.info(String.valueOf(System.identityHashCode(post)));
         em.clear();
-        post = postRepository.findByTitleEquals("Test Post 3").orElseThrow(() -> new EntityNotFoundException("Post not found"));
+        post = postRepository.findByTitleEquals("Test Post 1").orElseThrow(() -> new EntityNotFoundException("Post not found"));
         log.info(String.valueOf(System.identityHashCode(post)));
         log.info(String.valueOf(post.getComments().size()));
         assertThat(post.getComments()).isNotNull();
@@ -121,8 +139,9 @@ class PostRepositoryTest {
     }
 
     @Test
+    @Order(1)
     public void testGetPostDetail(){
-        Post post = postRepository.findByTitleEquals("Test Post 3").orElseThrow(() -> new EntityNotFoundException("Post not found"));
+        Post post = postRepository.findByTitleEquals("Test Post 1").orElseThrow(() -> new EntityNotFoundException("Post not found"));
         PostDetailDto postDetailDto = new PostDetailDto(post);
         for (CommentDto comment : postDetailDto.getComments()){
             log.info(String.valueOf(comment.getContent()));
@@ -130,8 +149,9 @@ class PostRepositoryTest {
     }
 
     @Test
+    @Order(1)
     public void testGetPostDetail2(){
-        Post post = postRepository.findByTitleEquals("Test Post 3").orElseThrow(() -> new EntityNotFoundException("Post not found"));
+        Post post = postRepository.findByTitleEquals("Test Post 1").orElseThrow(() -> new EntityNotFoundException("Post not found"));
         PostDetailDto postDetailDto = new PostDetailDto(post);
         JsonNode content = postDetailDto.getContent();
         JsonNode ops = content.get("ops");
@@ -140,16 +160,33 @@ class PostRepositoryTest {
     }
 
     @Test
+    @Order(1)
     public void getImages(){
-        Post post = postRepository.findByTitleEquals("Test Post 5").orElseThrow(() -> new EntityNotFoundException("Post not found"));
-        Set<String> images = imageService.getImageUrls(post.getJsonContent());
-        for(String image : images){
-            log.info(image);
+        Post post = postRepository.findByTitleEquals("Test Post 3").orElseThrow(() -> new EntityNotFoundException("Post not found"));
+        Set<PostImage> postImages = post.getPostImages();
+        log.info("PostImages size : " + postImages.size());
+    }
+
+    @Test
+    @Order(1)
+    public void updateImages(){
+        JsonNode jsonNode = jsonNodeOf("{\"ops\": [{\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=7.png\"}},{\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=8.png\"}}, {\"insert\": \"\\n\"}, {\"insert\": {\"image\": \"http://testtest/aas?preview=true&prefix=4.png\"}}]}");
+        Post post = postRepository.findByTitleEquals("Test Post 3").orElseThrow(() -> new EntityNotFoundException("Post not found"));
+        imageService.updatePostImage(post,jsonNode);
+        post.setJsonContent(jsonNode);
+        Set<PostImage> postImages = post.getPostImages();
+        for (PostImage postImage : postImages){
+            log.info("post's image is {}, and the status is {}", postImage.getImage().getFileName(), postImage.getStatus());
         }
+
+        imageRepository.findAll().forEach(image -> {
+            log.info("this is {}",image.getFileName());
+        });
     }
 
 
     @Test
     @Rollback(false)
+    @Order(2)
     public void saveTestedDate(){}
 }
