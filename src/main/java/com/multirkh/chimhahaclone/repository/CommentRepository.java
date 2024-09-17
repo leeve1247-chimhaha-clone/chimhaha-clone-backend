d656889fb3c6615eaabe8ea8830cd36c827ea109
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-17 14:04:47 +0900

    [게시글 목록] 댓글 갯수 조절 로직 추가

--- src/main/java/com/multirkh/chimhahaclone/repository/CommentRepository.java
+++ src/main/java/com/multirkh/chimhahaclone/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.multirkh.chimhahaclone.repository;
 
 import com.multirkh.chimhahaclone.entity.Comment;
+import com.multirkh.chimhahaclone.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Integer countCommentByPost(Post post);
+
 }
