6ed186b9192d9d042cef6e2b282241b20248b382
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-09-18 15:12:42 +0900

    feat(post-list): handle thumbnail image transfer with record

--- src/main/java/com/multirkh/chimhahaclone/service/image/resize/InputStreamAndLength.java
+++ src/main/java/com/multirkh/chimhahaclone/service/image/resize/InputStreamAndLength.java
@@ -1,15 +1,5 @@
 package com.multirkh.chimhahaclone.service.image.resize;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-
 import java.io.InputStream;
 
-@Getter
-@Setter
-@RequiredArgsConstructor
-public class InputStreamAndLength {
-    private int size;
-    private InputStream inputStream;
+public record InputStreamAndLength(int size, InputStream inputStream) {
 }
