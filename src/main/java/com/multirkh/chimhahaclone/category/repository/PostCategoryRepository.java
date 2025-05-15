6956ce99041b11724e941a0c04f952d795b6a014
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-15 14:48:05 +0900

    feat: update postcategory for router api

--- src/main/java/com/multirkh/chimhahaclone/category/repository/PostCategoryRepository.java
+++ src/main/java/com/multirkh/chimhahaclone/category/repository/PostCategoryRepository.java
@@ -1,20 +1,18 @@
 package com.multirkh.chimhahaclone.category.repository;
 
 import com.multirkh.chimhahaclone.category.entity.PostCategory;
-import com.multirkh.chimhahaclone.entity.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 
 @Repository
 public interface PostCategoryRepository extends JpaRepository<PostCategory, Long> {
     PostCategory findByKey(String key);
-
     List<PostCategory> findPostCategoriesByParent (PostCategory postCategory);
 
+    @Query("SELECT pc FROM PostCategory pc Where pc.id = (SELECT MAX(pc2.id) FROM PostCategory pc2 WHERE pc2.key = pc.key)")
+    List<PostCategory> findAllFlat();
 }
