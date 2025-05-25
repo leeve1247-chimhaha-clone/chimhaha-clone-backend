1c8bd9faf9ea12d8bfa470b8513fabcbc9e96c44
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-05-26 02:12:21 +0900

    feat: fix comment pagination

---
+++ src/main/java/com/multirkh/chimhahaclone/exception/GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.multirkh.chimhahaclone.exception;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.badRequest().body(new ErrorResponse() {
+
+            @NotNull
+            @Override
+            public HttpStatusCode getStatusCode() {
+                return HttpStatus.BAD_REQUEST;
+            }
+
+            @NotNull
+            @Override
+            public ProblemDetail getBody() {
+                ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+                problemDetail.setDetail(ex.getMessage());
+                return problemDetail;
+            }
+
+            @NotNull
+            @Override
+            public String getTypeMessageCode() {
+                return "";
+            }
+
+            @NotNull
+            @Override
+            public String getTitleMessageCode() {
+                return "";
+            }
+
+            @NotNull
+            @Override
+            public String getDetailMessageCode() {
+                return "";
+            }
+        });
+    }
+}
+
--- src/main/java/com/multirkh/chimhahaclone/repository/CommentRepository.java
+++ src/main/java/com/multirkh/chimhahaclone/repository/CommentRepository.java
@@ -21,20 +21,20 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query(value =  "with cte as (select id, " +
             "                    SUM(COALESCE(replies_count, 0) + 1) over (ORDER BY id) as accumulated_total " +
             "             from comments " +
-            "             where post_id = 6 " +
+            "             where post_id = :postId " +
             "               and parent_id is null), " +
-            "     cte2 as (select *, ceiling(accumulated_total / 5) as page from cte) " +
+            "     cte2 as (select *, ceiling(accumulated_total / :groupSize) as page from cte) " +
             "select min(id) as start_id, max(id) as end_id " +
             "from cte2\n " +
             "group by page;"
             , nativeQuery = true
     )
-    List<CommentPage> getCommentPages(@Param("postId") Long postId);
+    List<CommentPage> getCommentPages(@Param("postId") Long postId, @Param("groupSize") Integer groupSize);
 
-    @Query("select sum(coalesce(c.replies_count, 0))+count(*) from Comment c where c.parent is null and c.post = :post")
+    @Query("select sum(coalesce(c.replies_count, 0))+count(c) from Comment c where c.parent is null and c.post = :post")
     Integer countCommentsByPost(Post post);
 
-    @Query(value = "with RECURSIVE cte as (select c.* from comments c where (c.parent_id IS NULL and c.post_id = :postId and :startId <= c.id <= :endId) union all select t.* from comments t inner join cte on t.parent_id = cte.id) select * from cte;" , nativeQuery = true)
+    @Query(value = "with RECURSIVE cte as (select c.* from comments c where (c.parent_id IS NULL and c.post_id = :postId and :startId <= c.id and c.id <= :endId) union all select t.* from comments t inner join cte on t.parent_id = cte.id) select * from cte;" , nativeQuery = true)
     List<Comment> getRecursiveCommentsByStartEndId(@Param("startId") Long startId, @Param("endId") Long endId, @Param("postId") Long postId);
 
     @Query(value = "with RECURSIVE cte as (select c.* from comments c where (c.parent_id IS NULL and c.post_id = :postId) union all select t.* from comments t inner join cte on t.parent_id = cte.id) select * from cte;" , nativeQuery = true)
