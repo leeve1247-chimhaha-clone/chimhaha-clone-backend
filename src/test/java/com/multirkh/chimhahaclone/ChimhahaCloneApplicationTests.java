45b78fdfb1349aea68dde464f22cca1029ebb3d3
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-06-09 09:25:11 +0900

    feat: setup test environment

--- src/test/java/com/multirkh/chimhahaclone/ChimhahaCloneApplicationTests.java
+++ src/test/java/com/multirkh/chimhahaclone/ChimhahaCloneApplicationTests.java
@@ -1,13 +1,40 @@
 package com.multirkh.chimhahaclone;
 
+import com.multirkh.chimhahaclone.common.minio.MinioConfig;
+import com.multirkh.chimhahaclone.common.minio.MinioService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ChimhahaCloneApplicationTests {
+    @MockitoBean
+    MinioService minioService;
+    @MockitoBean
+    private MinioConfig minioConfig;
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
+
+    @MockitoBean
+    private RedisTemplate<String, String> redisTemplate;
+    @Mock
+    private ValueOperations<String, String> valueOperations;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void testSomething(){
+        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+        when(redisTemplate.opsForValue().get(anyString())).thenReturn("mockedValue");
+    }
 }
