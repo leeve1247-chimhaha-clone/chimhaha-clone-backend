5f7b9001743ba041f258cc65a3ca759cf3bfedad
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-06-18 12:07:10 +0900

    test: add test case template

---
+++ src/test/java/com/multirkh/chimhahaclone/api/comment/CommentServiceTest.java
@@ -0,0 +1,43 @@
+package com.multirkh.chimhahaclone.api.comment;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CommentServiceTest {
+
+    @Test
+    void createComment_shouldCreateNewCommentWithoutParent() {
+    }
+
+    @Test
+    void updateComment() {
+    }
+
+    @Test
+    void deleteComment() {
+    }
+
+    @Test
+    void updateCommentLikes() {
+    }
+
+    @Test
+    void validateCommentForm() {
+    }
+
+    @Test
+    void getCommentTree() {
+    }
+
+    @Test
+    void getCommentPageSize() {
+    }
+
+    @Test
+    void getCommentPage() {
+    }
+}
