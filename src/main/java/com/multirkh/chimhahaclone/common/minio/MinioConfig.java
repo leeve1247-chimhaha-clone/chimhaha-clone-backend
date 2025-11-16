0c6c0e2f130b85c2d5f0f90745089f2ca7e54cd4
Kihyun <69492686+leeve1247@users.noreply.github.com>
2025-11-16 13:11:58 +0900

    feat: minio 버전 업데이트

--- src/main/java/com/multirkh/chimhahaclone/common/minio/MinioConfig.java
+++ src/main/java/com/multirkh/chimhahaclone/common/minio/MinioConfig.java
@@ -1,19 +1,30 @@
 package com.multirkh.chimhahaclone.common.minio;
 
-import io.minio.*;
+import io.minio.BucketExistsArgs;
+import io.minio.DeleteBucketCorsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.SetBucketCorsArgs;
+import io.minio.messages.CORSConfiguration;
+import io.minio.messages.CORSConfiguration.CORSRule;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
 @Slf4j
 public class MinioConfig {
-    @Value("${minio.url}")
-    private String minioUrl;
+    @Value("${spa.web.origin}")
+    private String spaUrl;
+
+    @Value("${minio.export-url}")
+    private String minioExportUrl;
 
     @Value("${minio.access-key}")
     private String minioAccessKey;
@@ -28,26 +39,63 @@ public class MinioConfig {
 
     @Bean
     public MinioClient minioClient() {
+        MinioClient minioClient = initMinioConfig();
+        Map<String, Boolean> minioBuckets = initMinioBuckets(minioClient);
+        for (Map.Entry<String, Boolean> entry : minioBuckets.entrySet()) {
+            try {
+                minioClient.deleteBucketCors(DeleteBucketCorsArgs.builder().bucket(entry.getKey()).build());
+                CORSConfiguration config = getCorsConfiguration();
+                minioClient.setBucketCors(
+                        SetBucketCorsArgs.builder().bucket(entry.getKey()).config(config).build());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return minioClient;
+    }
+
+    private Map<String, Boolean> initMinioBuckets(MinioClient minioClient) {
         try {
-            MinioClient minioClient =
-                    MinioClient.builder()
-                            .endpoint(minioUrl)
-                            .credentials(minioAccessKey, minioSecretKey)
-                            .build();
-            Map<String, Boolean> founds = new HashMap<>();
-            founds.put(minioBucketName, minioClient.bucketExists(BucketExistsArgs.builder().bucket(minioBucketName).build()));
-            founds.put(thumbnailBucketName, minioClient.bucketExists(BucketExistsArgs.builder().bucket(thumbnailBucketName).build()));
-            for (Map.Entry<String, Boolean> entry : founds.entrySet()) {
+            Map<String, Boolean> bucketDoesExists = new HashMap<>();
+            bucketDoesExists.put(minioBucketName,
+                    minioClient.bucketExists(BucketExistsArgs.builder().bucket(minioBucketName).build()));
+            bucketDoesExists.put(thumbnailBucketName,
+                    minioClient.bucketExists(BucketExistsArgs.builder().bucket(thumbnailBucketName).build()));
+            for (Map.Entry<String, Boolean> entry : bucketDoesExists.entrySet()) {
                 if (!entry.getValue()) {
                     minioClient.makeBucket(MakeBucketArgs.builder().bucket(entry.getKey()).build());
                 } else {
                     log.info("Bucket '{}' already exists.", entry.getKey());
                 }
             }
-
-            return minioClient;
+            return bucketDoesExists;
         } catch (Exception e) {
             throw new RuntimeException("Error occurred while creating minio client", e);
         }
+    }
+
+    private MinioClient initMinioConfig() {
+        try {
+            return MinioClient.builder()
+                    .endpoint(minioExportUrl)
+                    .credentials(minioAccessKey, minioSecretKey)
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException("Minio Client Error");
+        }
+
+    }
+
+    private CORSConfiguration getCorsConfiguration() {
+        List<String> allowedOrigins = Collections.singletonList(spaUrl);
+        return new CORSConfiguration(
+                List.of(new CORSRule(
+                        List.of("*"),
+                        Arrays.asList("PUT", "POST"),
+                        allowedOrigins,
+                        null,
+                        null,
+                        3000))
+        );
     }
 }
